71687dca2cd120339e573733c0e4fd6d8052f3b4
Gabriel Valle <gvalle91@gmail.com>
2018-06-09 20:29:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
     return "FizzBuzz"
-  elseif number % 3 == 0
+  elsif number % 3 == 0
     return "Fizz"
-  elseif number % 5 == 0
+  elsif number % 5 == 0
     return "Buzz"
   else 
     return nil
